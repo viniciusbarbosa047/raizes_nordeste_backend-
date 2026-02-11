3aa0a9b250fa2d834b1743cff1f00d4fa2336f56
Vinicius Barbosa <dfdx047@unknown9e4cee624760.lan>
2026-02-11 13:54:27 +0000

    Adicionando regras de validacao de Pedidos e Itens

--- src/domain/schemas.py
+++ src/domain/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional
+from datetime import datetime
 
 # --- SCHEMAS (REGRAS DE DADOS) ---
 # Aqui eu defino o que o usuario manda e o que ele recebe de volta.
@@ -33,3 +34,40 @@ class ProdutoLer(ProdutoBase):
     
     class Config:
         from_attributes = True
+
+# --- REGRAS PARA PEDIDOS (O CORAÇÃO DO SISTEMA) ---
+
+# 3. Regras para ITENS DO PEDIDO (Ex: 2x Cuscuz)
+class ItemPedidoBase(BaseModel):
+    produto_id: int
+    quantidade: int
+
+class ItemPedidoCriar(ItemPedidoBase):
+    pass
+
+class ItemPedidoLer(ItemPedidoBase):
+    id: int
+    # Aqui vamos permitir mostrar o nome do produto junto, pra ficar chique
+    
+    class Config:
+        from_attributes = True
+
+# 4. Regras para O PEDIDO COMPLETO
+class PedidoBase(BaseModel):
+    unidade_id: int
+    forma_pagamento: str # PIX, MOCK, DINHEIRO
+    canal_pedido: str # APP, TOTEM, BALCAO
+
+class PedidoCriar(PedidoBase):
+    # O pedido TEM que ter uma lista de itens
+    itens: List[ItemPedidoCriar]
+
+class PedidoLer(PedidoBase):
+    id: int
+    status: str
+    valor_total: float
+    data_criacao: datetime = None # datetime precisa importar lá em cima
+    itens: List[ItemPedidoLer] = []
+
+    class Config:
+        from_attributes = True        
